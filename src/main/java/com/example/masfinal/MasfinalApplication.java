8159d7f0e3cb771ee076343c7d1eacacda2278a1
Tomasz Pyrzak <tpyrzak@wp.pl>
2022-06-21 19:21:48 +0200

    init commit

---
+++ src/main/java/com/example/masfinal/MasfinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.masfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasfinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasfinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/masfinal/MasfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.masfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasfinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
